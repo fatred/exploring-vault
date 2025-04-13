c155ab2134f9314b0a70073a288973f28493a6ae
John Howard <fatred@gmail.com>
2025-04-13 23:51:43 +0200

    make gnmi test work against nokia only

--- demo-env/show-version.py
+++ demo-env/show-version.py
@@ -1,8 +1,11 @@
 from nornir import InitNornir
+from nornir.core.filter import F
 from nornir_utils.plugins.functions import print_result
 from nornir_pygnmi.tasks import gnmi_get
 
 nr = InitNornir(config_file="config.yaml")
 
-result1 = nr.run(task=gnmi_get, encoding="json_ietf", path=["system/information/version"])
+srl = nr.filter(F(groups__contains="nokia_srlinux"))
+
+result1 = srl.run(task=gnmi_get, encoding="json_ietf", path=["system/information/version"])
 print_result(result1)
