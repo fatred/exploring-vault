35d7499112cc2b0813bbab0f846aa9ad24296a9f
John Howard <fatred@gmail.com>
2025-05-18 22:26:35 +0200

    typo fix

--- demo-env/test-pygnmi-vault.py
+++ demo-env/test-pygnmi-vault.py
@@ -4,7 +4,7 @@ import json
 
 # Variables
 port = 57400
-hosts = ['clab-demo-env-leaf1.problemofnetwork.com', 'clab-demo-env-leaf1.problemofnetwork.com', 'clab-demo-env-spine1.problemofnetwork.com']
+hosts = ['clab-demo-env-leaf1.problemofnetwork.com', 'clab-demo-env-leaf2.problemofnetwork.com', 'clab-demo-env-spine1.problemofnetwork.com']
 
 # Body
 if __name__ == '__main__':
@@ -12,4 +12,4 @@ if __name__ == '__main__':
         with gNMIclient(target=(host, port), username='admin', password='NokiaSrl1!') as gc:
              result = gc.get(path=['system/name'])
 
-        print(f"connected to {json.dumps(result['notification'][0]['update'][0]['val'])}")
+        print(f"connected with system certs via gnmi to {json.dumps(result['notification'][0]['update'][0]['val'])}")
