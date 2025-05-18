795402d55027406eb08b7d235b6de7b51d6b493f
John Howard <fatred@gmail.com>
2025-05-18 21:47:24 +0200

    add pathway to show insecure verify

--- demo-env/test-pygnmi-clab.py
+++ demo-env/test-pygnmi-clab.py
@@ -9,6 +9,7 @@ hosts = ['clab-demo-env-spine1', 'clab-demo-env-leaf1', 'clab-demo-env-leaf2']
 # Body
 if __name__ == '__main__':
     for host in hosts:
-        with gNMIclient(target=(host, port), username='admin', password='NokiaSrl1!', path_cert="clab-demo-env/.tls/ca/ca.pem") as gc:
+        #with gNMIclient(target=(host, port), username='admin', password='NokiaSrl1!', path_cert="clab-demo-env/.tls/ca/ca.pem") as gc:
+        with gNMIclient(target=(host, port), username='admin', password='NokiaSrl1!', verify=False) as gc:
              result = gc.get(path=['system/name'])    
-        print(f"connected to {json.dumps(result['notification'][0]['update'][0]['val'])}")
+        print(f"connected with clab certs on gnmi to {json.dumps(result['notification'][0]['update'][0]['val'])}")
