4fb88c8a339dbfb07ca8492a0eaf643b5660dfb6
John Howard <fatred@gmail.com>
2025-05-18 21:39:03 +0200

    split before and after pygnmi

---
+++ demo-env/test-pygnmi-clab.py
@@ -0,0 +1,14 @@
+# Modules
+from pygnmi.client import gNMIclient
+import json
+
+# Variables
+port = 57400
+hosts = ['clab-demo-env-spine1', 'clab-demo-env-leaf1', 'clab-demo-env-leaf2']
+
+# Body
+if __name__ == '__main__':
+    for host in hosts:
+        with gNMIclient(target=(host, port), username='admin', password='NokiaSrl1!', path_cert="clab-demo-env/.tls/ca/ca.pem") as gc:
+             result = gc.get(path=['system/name'])    
+        print(f"connected to {json.dumps(result['notification'][0]['update'][0]['val'])}")
---
+++ demo-env/test-pygnmi-vault.py
@@ -0,0 +1,15 @@
+# Modules
+from pygnmi.client import gNMIclient
+import json
+
+# Variables
+port = 57400
+hosts = ['clab-demo-env-leaf1.problemofnetwork.com', 'clab-demo-env-leaf1.problemofnetwork.com', 'clab-demo-env-spine1.problemofnetwork.com']
+
+# Body
+if __name__ == '__main__':
+    for host in hosts:
+        with gNMIclient(target=(host, port), username='admin', password='NokiaSrl1!') as gc:
+             result = gc.get(path=['system/name'])
+
+        print(f"connected to {json.dumps(result['notification'][0]['update'][0]['val'])}")
